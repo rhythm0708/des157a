20bec9932af745e758e362f4f1b7a425425f1fd3
rhythm07 <rhythmfever0708@gmail.com>
2024-11-04 01:10:41 -0800

    Finished HTML, CSS for Every Picture project

--- every-picture/script.js
+++ every-picture/script.js
@@ -2,6 +2,14 @@
     "use strict";
     console.log("reading js");
 
+    const form = document.querySelector("#case-questions");
+
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
+        alert("i haven't made this part yet :)");
+        // Add functionality here.
+    });
+
     // Manipulate icons.
     styleIcons();
 
@@ -10,3 +18,5 @@
         feather.replace();
     }
 })();
+
+// STRETCH GOAL: Add "story" overlay and tutorial screen.
